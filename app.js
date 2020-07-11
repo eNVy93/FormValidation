3ebf27ebb472bc468679c122b3f5aedb414c26af
Vytas Ne <netiksis.vytautas@gmail.com>
2020-07-11 14:53:27 +0300

    Form validation

---
+++ app.js
@@ -0,0 +1,39 @@
+// From BLur event listener
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatephone);
+
+function validateName(){
+  const name = document.getElementById("name");
+  const re = /^[a-zA-z]{2,10}$/;
+
+  if(!re.test(name.value)){
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+function validateZip(){
+  const name = document.getElementById("zip");
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+  if(!re.test(zip.value)){
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+}
+function validateEmail(){
+  const name = document.getElementById("email");
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if(!re.test(email.value)){
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+function validatephone(){
+
+}
